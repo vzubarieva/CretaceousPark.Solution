7e630b7cb024705ad7faa2f09e0dc0818e17ff15
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-28 11:18:14 -0700

    add update and delete routes

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
@@ -46,6 +49,57 @@ namespace CretaceousPark.Controllers
             }
 
             return animal;
+        }
+
+        // PUT: api/Animals/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _db.Animals.Any(e => e.AnimalId == id);
+        }
+
+        // DELETE: api/Animals/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            var animal = await _db.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _db.Animals.Remove(animal);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
