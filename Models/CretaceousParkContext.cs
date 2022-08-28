0d0e5332f6942584b52a12f0507cd29c9ee7968e
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-28 10:39:53 -0700

    add database setup

---
+++ Models/CretaceousParkContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CretaceousPark.Models
+{
+    public class CretaceousParkContext : DbContext
+    {
+        public CretaceousParkContext(DbContextOptions<CretaceousParkContext> options)
+            : base(options) { }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
